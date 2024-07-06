b6276fc2e735c511b219a5d117d0ece0e01b034e
Charitha Nanayakkara <49636974+charithananayakkara7@users.noreply.github.com>
2024-07-06 19:52:06 +0200

    changes to TestRunner class

--- src/test/java/cucmber/Options/TestRunner.java
+++ src/test/java/cucmber/Options/TestRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features =
        "src/test/java/feature files/ui/UserLogin.feature",
-        //"src/test/java/feature files/api/book_details_validation.feature",
-        //"src/test/java/feature files/api/createbook.feature",
+       // "src/test/java/feature files/api/book_details_validation.feature",
+       // "src/test/java/feature files/api/createbook.feature",
 
         plugin = {"pretty",
                 "html:target/cucumber-html-report",
