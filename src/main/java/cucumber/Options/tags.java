7b9ba272063ee93e8aa4718f86e2d3be9c6d50a5
Charitha Nanayakkara <49636974+charithananayakkara7@users.noreply.github.com>
2024-07-06 19:46:50 +0200

    changing naming convention

---
+++ src/main/java/cucumber/Options/tags.java
@@ -0,0 +1,11 @@
+package cucumber.Options;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+class tags {
+
+}
