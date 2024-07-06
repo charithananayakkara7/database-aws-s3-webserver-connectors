e56ec2512710025a1705b7fc3bd8c2f4f06eed4f
Charitha Nanayakkara <49636974+charithananayakkara7@users.noreply.github.com>
2024-07-06 19:47:39 +0200

    changes to User login test

--- src/test/java/stepdef/ui/UserLoginTest.java
+++ src/test/java/stepdef/ui/UserLoginTest.java
@@ -1,39 +1,28 @@
 package stepdef.ui;
-
-import Service.Awsconnector;
 import TestBase.BaseTest;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import io.cucumber.java.en.Then;
 import pageObjects.web.LoginPage;
-import resources.requestpayloads;
-
-import java.io.IOException;
-
-import static io.restassured.RestAssured.given;
 
 public class UserLoginTest extends BaseTest {
-    Logger logger = LogManager.getLogger(UserLoginTest.class);
-    @Given("Initialize the browser and redirection")
-    public void initialize_the_browser_with_chrome() {
-        initialize_browser();
+    @Given("I initialize the browser and redirection")
+    public void InitializeBrowser() {
+        initializeBrowser();
 
     }
 
 
-    @And("Navigate to the Home page {string}")
-    public void navigate_to_the_Home_page(String Email) throws Exception {
+    @And("I log in to the appplication")
+    public void navigateHomePage() throws Exception {
         LoginPage loginPage = new LoginPage(driver, TIME_OUT);
-            loginPage.clickSearchBtnn(Email);
-        logger.info("test sucessfully finished");
+           loginPage.clickloginBtn();
+           loginPage.doLogin();
         }
 
-
-
-    @And("^close browsers")
-    public void close_browsers() throws Throwable {
+    @Then("I close the browser")
+    public void closeBrowser() throws Exception {
         browserclose();
-    }
+            }
 }
 
