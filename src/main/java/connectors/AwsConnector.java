dc5020a60abe34c94ef22b0dc0ce2def317a17e9
Charitha Nanayakkara <49636974+charithananayakkara7@users.noreply.github.com>
2024-07-07 12:18:30 +0200

    changes to aws connector classes

--- src/main/java/connectors/AwsConnector.java
+++ src/main/java/connectors/AwsConnector.java
@@ -1,4 +1,4 @@
-package Service;
+package connectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/connectors/OracleDBConnector.java
+++ src/main/java/connectors/OracleDBConnector.java
@@ -1,4 +1,4 @@
-package Service;
+package connectors;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
