e29dcf5225c8f49ea4d1e400b57cbc2a461e2643
Charitha Nanayakkara <49636974+charithananayakkara7@users.noreply.github.com>
2024-07-07 12:49:43 +0200

    adding new config  loader class

---
+++ src/main/java/utils/ConfigLoader.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private Properties properties = new Properties();
+
+    public ConfigLoader(String propertiesFileName) {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(propertiesFileName)) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + propertiesFileName);
+                return;
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static void main(String[] args) {
+        ConfigLoader configLoader = new ConfigLoader("config.properties");
+        System.out.println("QA Base URL: " + configLoader.getProperty("BaseUrlQA"));
+        System.out.println("Dev Base URL: " + configLoader.getProperty("BaseUrlDev"));
+    }
+}
